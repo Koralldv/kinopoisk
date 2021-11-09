cbce206b99fdd03af7816508fa8db34eea779456
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-09 19:13:57 +0300

    Подключил шапку к приложению

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import {Header} from './components/Header'
+import React from 'react';
 
 function App() {
+
   return (
     <div className="App">
-      Hellp
+      <Header/>
     </div>
   );
 }
