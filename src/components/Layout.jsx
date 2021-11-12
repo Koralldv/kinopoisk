96f4be94bf332d39d0ba48bd29b2ce8254b04650
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-13 02:33:31 +0300

    Добавил задел внутренних страниц

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+
+export const Layout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            footer
+        </>
+    );
+};
