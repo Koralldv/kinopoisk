5642ae8e46f9b6730a64d6cd30e10a16be575657
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-13 02:37:47 +0300

    Органнизация роутинга страниц

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import {Header} from './components/Header'
-import {MainSlider} from './components/MainSlider'
-import {Featured} from './components/Featured'
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import {Home} from './Pages/Home'
+import {About} from './Pages/About'
+import {Contact} from './Pages/Contact'
+import {NotFound} from './Pages/NotFound'
+import {Layout} from './components/Layout'
 
 function App() {
 
   return (
     <div className="App">
-      <Header/>
-      <MainSlider/>
-      <Featured/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='contact' element={<Contact/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
