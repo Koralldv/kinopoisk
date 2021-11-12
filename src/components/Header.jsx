a5368effd937b10fb8f7cb8aaae2bc558dd47b23
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-13 02:36:06 +0300

    Перевел обычные ссылки на NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,55 @@
 import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
 import { IoSearchSharp, IoMoon, IoMoonOutline, IoPerson } from 'react-icons/io5';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return (
+        <Wrapper>
+            <Head>
+                <Menu>
+                    <NavLink to="/">
+                        <MenuItem>HOME</MenuItem>
+                    </NavLink>
+                    <NavLink to="/movie">
+                        <MenuItem>MOVIES</MenuItem>
+                    </NavLink>
+                    <NavLink to="/shows">
+                        <MenuItem>SHOWS</MenuItem>
+                    </NavLink>
+                    <NavLink to="/about">
+                        <MenuItem>ABOUT</MenuItem>
+                    </NavLink>
+                </Menu>
+                <SideMenu>
+                    <MenuItem>
+                        <IoSearchSharp size="20px" />
+                    </MenuItem>
+                    <MenuItem>
+                        <IoPerson size="20px" />
+                    </MenuItem>
+                    <MenuItem onClick={toggleTheme}>
+                        <ModeSwitcher>
+                            {theme === 'light' ? (
+                                <IoMoonOutline size="20px" />
+                            ) : (
+                                <IoMoon size="20px" />
+                            )}
+                        </ModeSwitcher>
+                    </MenuItem>
+                </SideMenu>
+            </Head>
+        </Wrapper>
+    );
+};
 
 const Wrapper = styled.div`
     width: 100%;
@@ -14,6 +63,7 @@ const Head = styled.header`
     display: flex;
     flex-direction: column;
     align-items: center;
+    height: 50px;
     @media (min-width: 767px) {
         justify-content: space-between;
         flex-direction: row;
@@ -25,13 +75,11 @@ const Menu = styled.ul`
     list-style: none;
     padding: 0;
     margin: 0;
+    align-items: center;
 `;
 
-const MenuItem = styled.a.attrs({
-    href: '#',
-})`
+const MenuItem = styled.li`
     padding: 1rem;
-    text-decoration: none;
     color: var(--colors-text);
     cursor: pointer;
     transition: background 0.2s;
@@ -57,43 +105,3 @@ const ModeSwitcher = styled.div`
     font-weight: var(--fw-bold);
     text-transform: capitalize;
 `;
-
-export const Header = () => {
-    const [theme, setTheme] = useState('light');
-
-    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
-
-    useEffect(() => {
-        document.body.setAttribute('data-theme', theme);
-    }, [theme]);
-
-    return (
-        <Wrapper>
-            <Head>
-                <Menu>
-                    <MenuItem>HOME</MenuItem>
-                    <MenuItem>MOVIES</MenuItem>
-                    <MenuItem>SHOWS</MenuItem>
-                    <MenuItem>ABOUT</MenuItem>
-                </Menu>
-                <SideMenu>
-                    <MenuItem>
-                        <IoSearchSharp size="20px" />
-                    </MenuItem>
-                    <MenuItem>
-                        <IoPerson size="20px" />
-                    </MenuItem>
-                    <MenuItem onClick={toggleTheme}>
-                        <ModeSwitcher>
-                            {theme === 'light' ? (
-                                <IoMoonOutline size="20px" />
-                            ) : (
-                                <IoMoon size="20px" />
-                            )}
-                        </ModeSwitcher>
-                    </MenuItem>
-                </SideMenu>
-            </Head>
-        </Wrapper>
-    );
-};
