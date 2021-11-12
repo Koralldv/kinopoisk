f20ca3d65b227c828d934ea93cbdd9bbb06e7f11
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-13 02:37:08 +0300

    Вынес компоненты главной страницы в отдельных файл

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainSlider } from '../components/MainSlider';
+import { Featured } from '../components/Featured';
+
+export const Home = () => {
+    return (
+        <>
+            <MainSlider />
+            <Featured />
+        </>
+    );
+};
