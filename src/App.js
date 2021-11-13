3caef1d7b8ff8075872bab8aa82154469240d09a
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-13 23:23:30 +0300

    Добавил динамическую страницу фильма в роутинг

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {About} from './Pages/About'
 import {Contact} from './Pages/Contact'
 import {NotFound} from './Pages/NotFound'
 import {Layout} from './components/Layout'
+import {Film} from './Pages/Film'
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
           <Route index element={<Home/>} />
           <Route path='about' element={<About/>} />
           <Route path='contact' element={<Contact/>} />
+          <Route path='film' element={<Film/>}>
+            <Route path=':filmId' element={<Film/>}/>
+          </Route>
           <Route path='*' element={<NotFound/>} />
         </Route>
       </Routes>
