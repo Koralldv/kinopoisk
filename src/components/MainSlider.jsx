aa906a7f6bb0a429acc59ecf1c3d6ee6095d04b0
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-15 13:03:21 +0300

    Добавил стрелки слайдеру

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -1,11 +1,57 @@
 import styled from 'styled-components';
 import React from 'react';
-// Import Swiper React components
+
+import { IoArrowForwardOutline, IoArrowBack } from 'react-icons/io5';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
+import SwiperCore, { Navigation } from 'swiper/core';
 
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
+
+SwiperCore.use([Navigation]);
+
+export const MainSlider = () => {
+    return (
+        <Swiper
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation={{ nextEl: '.main__next', prevEl: '.main__prev' }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}>
+            {filmsSlider.map((item, index) => (
+                <SwiperSlide key={`${item.title}_${index}`}>
+                    <Img src={item.img} />
+                    <Wrapper>
+                        <Rgba_bg />
+                        <Film>
+                            <FilmTitle>{item.title}</FilmTitle>
+
+                            {item.list.map((listItem, index) => (
+                                <Digits key={`${listItem}_${index}`}>
+                                    <Year>{listItem.year}</Year>
+                                    <Age>{listItem.age}</Age>
+                                    <Duration>{listItem.duration}</Duration>
+                                </Digits>
+                            ))}
+                            <Desc>{item.desc}</Desc>
+                            <Buttons>
+                                <Button>Play now</Button>
+                                <Button>+ My list</Button>
+                            </Buttons>
+                        </Film>
+                    </Wrapper>
+                </SwiperSlide>
+            ))}
+            <Arrow left className="main__prev">
+                <IoArrowBack size="22px" />
+            </Arrow>
+            <Arrow right className="main__next">
+                <IoArrowForwardOutline size="22px" />
+            </Arrow>
+        </Swiper>
+    );
+};
 
 const Wrapper = styled.div`
     width: 100vw;
@@ -98,6 +144,24 @@ const Button = styled.button`
     color: var(--color-text);
 `;
 
+const Arrow = styled.span`
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    top: 40%;
+    background-color: var(--rgba);
+    box-shadow: var(--shadow);
+    z-index: 99999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50px;
+    cursor: pointer;
+
+    left: ${(props) => (props.left ? '5px' : 'auto')};
+    right: ${(props) => (props.right ? '5px' : 'auto')};
+`;
+
 const filmsSlider = [
     {
         title: 'THE EARTH',
@@ -118,38 +182,3 @@ const filmsSlider = [
         list: [{ year: '2021', age: '18', duration: '2h 6m' }],
     },
 ];
-
-export const MainSlider = () => {
-    return (
-        <Swiper
-            spaceBetween={0}
-            slidesPerView={1}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}>
-            {filmsSlider.map((item, index) => (
-                <SwiperSlide key={`${item.title}_${index}`}>
-                    <Img src={item.img} />
-                    <Wrapper>
-                        <Rgba_bg />
-                        <Film>
-                            <FilmTitle>{item.title}</FilmTitle>
-
-                            {item.list.map((listItem, index) => (
-                                <Digits key={`${listItem}_${index}`}>
-                                    <Year>{listItem.year}</Year>
-                                    <Age>{listItem.age}</Age>
-                                    <Duration>{listItem.duration}</Duration>
-                                </Digits>
-                            ))}
-                            <Desc>{item.desc}</Desc>
-                            <Buttons>
-                                <Button>Play now</Button>
-                                <Button>+ My list</Button>
-                            </Buttons>
-                        </Film>
-                    </Wrapper>
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
-};
