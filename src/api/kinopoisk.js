88fc175dc8b9cd6cf4ca4de8034388ee8ed2af9f
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-17 01:44:45 +0300

    Добавил api кинопоиска

---
+++ src/api/kinopoisk.js
@@ -0,0 +1,15 @@
+export const BASE_URL_22 = 'https://kinopoiskapiunofficial.tech/api/v2.2/films';
+
+export const BASE_URL_21 = 'https://kinopoiskapiunofficial.tech/api/v2.1/films';
+
+let formatter = new Intl.DateTimeFormat('en-GB', { month: 'long' });
+let monthNow = formatter.format(new Date()).toUpperCase().toString();
+
+let date = new Date();
+let yearNow = date.getFullYear().toString();
+
+export let PREMIERE = BASE_URL_22 + `/premieres?year=${yearNow}&month=${monthNow}`;
+
+export const SINGLE_FILM = (id) => BASE_URL_22 + '/' + id
+
+export const FRAMES = (id) => BASE_URL_21 + `/${id}/frames`
