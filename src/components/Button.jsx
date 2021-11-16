f48313530673aeb4d89076553698a364b092b147
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-17 01:45:01 +0300

    Вынес кнопку в отдельный компонент

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    background-color: var(--colors-ui-base);
+    border: none;
+    border-radius: var(--radii);
+    box-shadow: var(--shadow);
+    padding: 1rem 2rem;
+    cursor: pointer;
+    margin: 1rem 1rem 1rem 0;
+    color: var(--color-text);
+`;
