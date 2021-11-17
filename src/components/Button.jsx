412b9fc51d5be556fc608f185a08e7f131b903c8
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-11-17 03:14:41 +0300

    Добавил толщину шрифта

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-    background-color: var(--colors-ui-base);
-    border: none;
     border-radius: var(--radii);
     box-shadow: var(--shadow);
+    color: var(--color-text);
+    background-color: var(--colors-ui-base);
+    font-weight: var(--fw-normal);
+    border: none;
     padding: 1rem 2rem;
     cursor: pointer;
     margin: 1rem 1rem 1rem 0;
-    color: var(--color-text);
 `;
