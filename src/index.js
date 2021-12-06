517992ade51e444ec23157861e46ccccbe158dc0
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-12-06 15:03:55 +0300

    Подключение стора

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import store from './store'
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
@@ -7,7 +9,11 @@ import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+        <Provider store={store}>
+          <div className="App">
+            <App />
+          </div>
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
