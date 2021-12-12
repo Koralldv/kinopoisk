126e387a7adc02cca9ea94d3532fe80f5bece857
Koralldv <52466177+Koralldv@users.noreply.github.com>
2021-12-13 02:13:37 +0300

    Подключил редусер

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import premierReducer from './premierSlice'
 import topReducer from './topSlice'
 import bestReducer from './bestSlice'
 import popularReducer from './popularSlice'
+import likeReducer from './likeSlice'
 
 export default configureStore({
     reducer: {
         premier: premierReducer,
         top: topReducer,
         best: bestReducer,
-        popular: popularReducer
+        popular: popularReducer,
+        like: likeReducer
     }
 })
